2579db3446e5f5d86077d1b6e51d379e85649fc2
mezleca <mtksenpai@gmail.com>
2025-09-23 01:21:26 -0300

    get correct mouse key

--- js/events/dom.js
+++ js/events/dom.js
@@ -1,8 +1,12 @@
-// DOM events, setup render main loop
-
 export const keys = new Set();
 export const screen = { w: 1920, h: 1080 };
 export const cursor = { x: 0, y: 0, delta_y: 0, delta_x: 0 };
+
+const MBUTTON_NAMES = {
+    0: "mouse1",
+    1: "mouse3",
+    2: "mouse2"
+};
 
 window.addEventListener("mousemove", (e) => {
     cursor.x = e.clientX;
@@ -14,12 +18,18 @@ window.addEventListener("wheel", (e) => {
     cursor.delta_x = e.deltaX;
 });
 
-window.addEventListener("mousedown", () => {
-    keys.add("mouse1");
+window.addEventListener("mousedown", (e) => {
+    const name = MBUTTON_NAMES[e.button];
+    if (name) keys.add(name);
 });
 
-window.addEventListener("mouseup", () => {
-    keys.delete("mouse1");
+window.addEventListener("mouseup", (e) => {
+    const name = MBUTTON_NAMES[e.button];
+    if (name) keys.delete(name);
+});
+
+window.addEventListener("blur", () => {
+    keys.clear();
 });
 
 export const update_viewport = (canvas) => {
